88f6f0d22950836777496c6761c80d15cd1d5f91
kumar <pavansanneboina080@gmail.com>
2026-07-21 15:04:40 +0530

    two files added

--- app.sh
+++
@@ -1,5 +0,0 @@
-echo "i am learning shell script"
-echo $0
-echo "you are name is pavan"
-echo "i want to know about git diff command"
-i am adding git fetch command how its work on my laptop.
---
+++ testing.sh
@@ -0,0 +1,1 @@
+"i am creating this file how to work in the directory"
